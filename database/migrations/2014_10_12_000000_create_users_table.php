969e4341dd7ca49466bf6c89c79f7b1bcd17a3bf
him123hey <heyhim99@gmail.com>
2021-12-03 18:03:21 +0700

    #1 add register feature

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger("role_id");
-            $table->foreign("role_id")->references('id')->on('roles')->onDelete(('cascade'));
+            $table->foreign("role_id")->references('id')->on('roles')->onDelete(('cascade'))->default('2');
             $table->rememberToken();
             $table->timestamps();
         });
